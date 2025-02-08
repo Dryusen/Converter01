a22898d7dfb11971edb4fc581a3adcc34bbdfddc
Dryusen <masalovandreyn@gmail.com>
2025-02-08 13:01:15 +0200

    Add conversion from miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My SSH app.");
+        double miles = 5;
+        double kilometers = milesToKilometers(miles);
+        System.out.println(miles + " миль = " + kilometers + " кілометрів");
+    }
+
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
     }
 }
